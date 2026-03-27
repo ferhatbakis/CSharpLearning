a637f9a1a34b96b727fd824d2eee4ff64a34e109
ferhatbakis <bakisferhat@gmail.com>
2026-03-27 23:35:28 +0300

    C#

---
+++ 09_DatabaseProject/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DatabaseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ado net
+            Console.WriteLine("Veritabanı Projesine Hoşgeldiniz!");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNumber;
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış yap");
+            Console.Write("Lütfen tablo numarasını giriniz: ");
+            tableNumber = Console.ReadLine();
+            Console.WriteLine("-----------------------------------");  
+            
+            var connectionString = "Data Source=.\\SQLEXPRESS03;Initial Catalog=EgitimKampiDb;Integrated Security=True";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (var command = new SqlCommand("Select * From TblCategory", connection))
+                using (var adapter = new SqlDataAdapter(command))
+                {
+                    var dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        foreach (var item in row.ItemArray)
+                        {
+                            Console.Write(item.ToString());
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
